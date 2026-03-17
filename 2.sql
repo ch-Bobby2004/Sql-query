8939ae11f411c1784f533e23808b4c8a1fc70e24
Bobby <choudharybobby14nov@gmail.com>
2026-03-16 22:44:58 -0700

    Added new files

--- 2.sql
+++ 2.sql
@@ -23,3 +23,25 @@ FROM students;
 -- 
 SELECT country, min(age),max(age),avg(age)
 FROM Customers group by country;
+
+
+-- 
+SELECT ROUND(12.567, 2);
+SELECT ROUND(12.563, 2);
+SELECT TRUNC(12.567, 2);
+
+
+SELECT TRUNC(9876, -1);  -- 9870
+SELECT TRUNC(9876, -2);  -- 9800
+SELECT TRUNC(9876, -3);  -- 9000
+
+
+-- 
+SELECT GENERATE_SERIES(1, 10);
+
+SELECT GENERATE_SERIES(1, 10, 2);
+SELECT GENERATE_SERIES(
+DATE '2024-01-01',
+DATE '2024-01-10',
+INTERVAL '1 day'
+);
